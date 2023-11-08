c71489ad65d71056487aef824ddab11f235d6515
yasin mesekoparan <y.mesekoparan1510@gmail.com>
2023-11-09 00:03:07 +0300

    08.11

--- src/test/java/HomeWorks/HomeWork5.java
+++ src/test/java/HomeWorks/HomeWork5.java
@@ -1,8 +1,16 @@
 package HomeWorks;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class HomeWork5 {
 
-    public static void main(String[] args) {
+
         /*
              ÖDEV:
              https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
@@ -10,5 +18,40 @@ public class HomeWork5 {
              Delete butonuna 90 kez basınız
              Ve 90 kez basıldığını doğrulayınız
       */
-    }
+        public static void main(String[] args) throws InterruptedException {
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+//https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
+            driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+
+//Add Element butonuna 100 kez basin
+            WebElement addButton = driver.findElement(By.cssSelector("button[onclick='addElement()']"));
+            for (int i = 0; i < 100; i++) {
+                addButton.click();
+                Thread.sleep(100);
+            }
+
+//Delete butonu’na  90 kez basin
+
+            for (int i = 0; i < 90; i++) {
+                driver.findElement(By.xpath("(//*[.='Delete'])[1]")).click();
+                Thread.sleep(100);
+            }
+
+//Ve 90 kez basıldığını doğrulayınız
+
+
+            int actualSize = driver.findElements(By.xpath("//*[.='Delete']")).size();
+            int expectedSize = 10;
+            boolean result = actualSize == expectedSize;
+            System.out.println(result ? "TEST PASSED" : " TEST FAILED");
+
+//sayfayı kapatınız
+            driver.quit();
+
+        }
+
 }
---
+++ src/test/java/HomeWorks/HomeWorkEbay.java
@@ -0,0 +1,9 @@
+package HomeWorks;
+
+public class HomeWorkEbay {
+            //    Yeni odevi
+            //- ebay sayfasına gidiniz
+            //- electronics bölümüne tıklayınız
+            //- Genişliği 225 ve Uzunluğu 225 olan resimlerin hepsine tıklayalım
+            //- Her sayfanın sayfa başlığını yazdıralım
+}
